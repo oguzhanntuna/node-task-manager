b08e678a40103e91d3dd249c77e76bcf57681a0c
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-11 19:25:58 -0700

    resource reading endpoints

--- src/index.js
+++ src/index.js
@@ -12,13 +12,29 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save()
-        .then(() => {
-            res.status(201).send(user);
-        })
-        .catch(error => {
-            res.status(400).send(error);
-        });
+        .then(() => res.status(201).send(user))
+        .catch(error => res.status(400).send(error));
 });
+
+app.get('/users', (req, res) => {
+    User.find({})
+        .then(users => res.send(users))
+        .catch(error => res.status(500).send());
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send();
+            }
+
+            res.send(user);
+        })
+        .catch(error => res.status(500).send());
+})
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
@@ -31,6 +47,26 @@ app.post('/tasks', (req, res) => {
             res.status(400).send(error);
         });
 });
+
+app.get('/tasks', (req, res) => {
+    Task.find({})
+        .then(tasks => res.send(tasks))
+        .catch(error => res.status(500).send());
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id)
+        .then(task => {
+            if (!task) {
+                return res.status(404).send();
+            }
+
+            res.send(task);
+        })
+        .catch(error => res.status(500).send());
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
