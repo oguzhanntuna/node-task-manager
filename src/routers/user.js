45b7ccd5cd2f05472137fafefab9d1a51ed7273c
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-13 14:58:05 -0700

    hiding private data with Scheme.methods.toJSON

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,6 +15,17 @@ router.post('/users', async (req, res) => {
     }
 });
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+
+        res.send({ user, token });
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
@@ -34,17 +45,6 @@ router.post('/users/logoutAll', auth, async (req, res) => {
         res.send();
     } catch (error) {
         res.status(500).send();
-    }
-});
-
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        const token = await user.generateAuthToken();
-
-        res.send({ user, token });
-    } catch (error) {
-        res.status(400).send();
     }
 });
 
