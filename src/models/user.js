a10904fa2314f13a58a74c5f8e437f8ef07bfa75
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-13 09:39:41 -0700

    logging in users

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     }, 
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowerCase: true,
@@ -41,7 +42,23 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// hashing password before saving the user data
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login!');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login!');
+    }
+
+    return user;
+}
+
+// hashing plain text password before saving the user data
 userSchema.pre('save', async function (next) {
     const user = this;
 
@@ -50,7 +67,7 @@ userSchema.pre('save', async function (next) {
     }
 
     next();
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
