701cd44da0d93e7a047282d860541a5f8c44524e
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-12 17:14:36 -0700

    resource deleting endpoints added

--- src/index.js
+++ src/index.js
@@ -67,6 +67,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 });
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user){
+            res.send(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
@@ -123,6 +137,20 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task);
     } catch (error) {
         res.status(400).send(error);
+    }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(500).send();
     }
 });
 
